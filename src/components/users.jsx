0e383616d869dcddf022e7098ac31c9a2968a4e1
CheeseCheesson <kira7999@gmail.com>
2021-12-03 08:28:19 +0300

    add dynamic data output in the table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,48 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../api";
 
 export const Users = () => {
-  const users = api.users.fetchAll();
+  const [users, setUsers] = useState(api.users.fetchAll());
 
-  const userName = users[1].name;
-  console.log(userName);
-  const userProfessions = users[1].profession.name;
-  console.log(userName + " " + userProfessions);
-
-  const render = () => {};
+  const handleDelete = (userId) => {};
+  const renderPhrase = (number) => {};
+  const getBageClasses = (color) => {
+    let bgColorQuality = "btn-sm m-1 text-white fw-bolder text-center btn-";
+    bgColorQuality += color;
+    return bgColorQuality;
+  };
 
   return (
     <>
-    <h1> <span class="badge bg-primary">12 человек тусанёт с тобой сегодня</span></h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качества</th>
-          <th scope="col">Профессия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-          <th scope="col">Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Джон Дориан</td>
-          <td>
-            <ul>
-              <li className="btn btn-primary  btn-sm m-1">Нудила</li>
-              <li className="btn btn-dark  btn-sm m-1">Неуверенный</li>
-              <li className="btn btn-secondary  btn-sm m-1">Странный</li>
-            </ul>
-          </td>
-          <td>Доктор</td>
-          <td>38</td>
-          <td>2.5/5</td>
-          <td> <span className='btn btn-lg btn-danger'>delete</span></td>
-        </tr>
+      <h1>
+        {" "}
+        <span className="badge bg-primary">
+          12 человек тусанёт с тобой сегодня
+        </span>
+      </h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col">Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((item) => (
+            <>
+              <tr key={item._id}>
+                <td>{item.name}</td>
 
-      </tbody>
-    </table>
-  </>
+                <td>
+                  {item.qualities.map((quality) => (
+                    <span
+                      className={getBageClasses(quality.color)}
+                      key={quality._id}
+                    >
+                      {quality.name}
+                    </span>
+                  ))}
+                </td>
+
+                <td>
+                  <span key={item.profession._id}>{item.profession.name}</span>
+                </td>
+                <td>{item.completedMeetings}</td>
+                <td>{item.rate}</td>
+                <td>
+                  <span className="btn btn-lg btn-danger">delete</span>
+                </td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
